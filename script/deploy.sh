ed8330d1801a8e3f569f749c722e5bc24521d7cb
Bin Yong Min <binyong318@hanssem.com>
2023-10-26 16:21:10 +0900

    배포 테스트

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-IS_FIRST=$(docker ps | (grep blue & grep green)) # 현재 구동중인 App이 있는지 확인
-IS_GREEN=$(docker ps | grep blue) # 현재 실행중인 App이 blue인지 확인
+IS_FIRST=$(docker ps | grep -e blue -e green) # 현재 구동중인 App이 있는지 확인
+IS_GREEN=$(docker ps | grep green) # 현재 실행중인 App이 blue인지 확인
 DEFAULT_CONF=" /etc/nginx/nginx.conf"
 
 if [ -z $IS_GREEN ];then # blue라면
@@ -14,7 +14,7 @@ if [ -z $IS_GREEN ];then # blue라면
   echo "2. green container up"
   docker-compose up -d green # green 컨테이너 실행
 
-  if [ -z $IS_FIRST ];then # 해당 조건을 넣지 않으면 처음 구동 시 무한 루프 발생
+  if [ -n $IS_FIRST ];then # 해당 조건을 넣지 않으면 처음 구동 시 무한 루프 발생
       echo "first start"
   else
     while [ 1 = 1 ]; do
@@ -33,7 +33,7 @@ if [ -z $IS_GREEN ];then # blue라면
   sudo cp /etc/nginx/nginx.green.conf $DEFAULT_CONF
   sleep 5 # 복사 후 바로 nginx 재시작하면 에러 발생
 
-  sudo nginx -s rel # nginx 재시작
+  sudo nginx -s reload # nginx 재시작
 
   echo "5. blue container down"
   docker-compose stop blue
